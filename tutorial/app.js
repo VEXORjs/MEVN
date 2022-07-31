4f4c33cfe1f71d7756a63399f63fc10e869bdf4c
Kacper Adamczyk <kacperadam10@gmail.com>
2022-07-31 23:21:46 +0200

    Update app.js

--- tutorial/app.js
+++ tutorial/app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createHttpError(404));
-  res.write('Somethig went wrong');
+  res.write('Something went wrong');
 });
 
 // error handler
